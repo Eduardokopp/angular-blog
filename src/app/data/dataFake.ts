0f273fab1c4686250a1eeca9d4187b585556b6ee
Eduardo Kopp Santos <eduardokopp3366@gmail.com>
2023-05-01 15:06:19 -0300

    Fix: Alterações das informaçoes espostas no BLOG
    Alterações das informaçoes espostas no BLOG

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "JS-DEVELOPER-POKEDEX by DIO.ME",
+    "description": "Projeto desenvolvido como exercício do bootcamp Potência Tech Angular Developer - Powered by iFood , com intuíto de prática de JavaScript. Foi disponibilizado o projeto com a listagem de pokémons, e o exercício proposto era melhorar a página, e implementar a tela de detalhes dos pokémons.",
+    "photoCover":"https://th.bing.com/th/id/R.36e05e32042bbae6a17da30374fca8fd?rik=f5msXMBisZ9lqA&riu=http%3a%2f%2fimg.huffingtonpost.com%2fasset%2f2000_1000%2f5798cd812400002600b3361b.png&ehk=dCqZsfdh6kCaPYIDYS1rcouHsA2pkCQ9RgeaAF5CpVE%3d&risl=&pid=ImgRaw&r=0"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Curriculo vitae HTML e CSS",
+    "description": "O projeto tem como objetivo montar nossa primeira página web que será a replica de um CV Online, utilizando conceitos de html e Github Pages. Ao fim teremos nosso currículo pronto e disponível de forma estática.",
+    "photoCover":"https://img.freepik.com/fotos-gratis/icone-de-pasta-com-papel-em-um-fundo-branco_476612-9319.jpg?size=626&ext=jpg"
+  },
+  {
+    "id":"3",
+    "title": "README.md",
+    "description": "Meu readme criado com algumas referencias",
+    "photoCover":"https://th.bing.com/th/id/OIP.Wyrw2EbvjIf7x-lOZFI2_QHaD4?pid=ImgDet&w=1200&h=630&rs=1"
+  },
+  {
+    "id":"4",
+    "title": "Pagina de checkout em Bootstrap",
+    "description": "Pagina de checkout dinamica em HTML com o framewor Bootstrap",
+    "photoCover":"https://user-images.githubusercontent.com/93801467/191874536-97b1d1dc-a084-4840-90e3-372798417c77.png"
   }
 ]
